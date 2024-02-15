982b249f99fb5ae7876959c270080928054fc8c1
Ismail-Iyada <ismailiyada@gmail.com>
2024-02-15 15:46:15 +0100

    signUp page UI

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,14 @@
+// * Define a function called errorHandler that takes in two parameters: statusCode and message
 export const errorHandler = (statusCode, message) => {
-  const error = new Error();
-  error.statusCode = statusCode;
-  error.message = message;
-  return error;
+    // ?Create a new instance of the Error object and assign it to the variable 'error'
+    const error = new Error();
+
+    // ? Set the 'statusCode' property of the 'error' object to the value of the 'statusCode' parameter
+    error.statusCode = statusCode;
+
+    // ? Set the 'message' property of the 'error' object to the value of the 'message' parameter
+    error.message = message;
+
+    // ! Return the 'error' object
+    return error;
 };
