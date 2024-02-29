c94fccb5f762c8981c69be2beceb05aab93752d5
Ismail-Iyada <ismailiyada@gmail.com>
2024-02-29 22:30:43 +0100

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  test,
+  updateUser,
+  getUserListings,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 // * use express to create the route
@@ -10,4 +15,5 @@ router.get("/test", test);
 // ? we can pass other route parameters in the URL and access them in the route handler using req.params. like so: /update/:id/:name
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 export default router;
