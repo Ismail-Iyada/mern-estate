8300f2598619deeb76c098841763857604f42365
Ismail-Iyada <ismailiyada@gmail.com>
2024-03-13 07:23:39 +0000

    complete About page && app title/icon

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className=" mx-auto max-w-6xl px-4 py-20">
+      <h1 className="mb-4 text-2xl font-bold text-slate-800 sm:text-3xl">
+        About <span className="text-slate-600">Iyada</span> Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod natus
+        repudiandae laudantium, placeat sit omnis reprehenderit a in, amet
+        veniam, ea repellat aliquid laborum autem similique repellendus porro ad
+        voluptates. Fugiat assumenda, nesciunt aliquid vitae ea est! Vel,
+        corrupti est rem neque nemo debitis veritatis in labore vitae tempore?
+        Quam aut adipisci reiciendis illum neque fugiat officia illo mollitia
+        vitae.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque optio
+        laudantium alias nesciunt maiores doloremque corrupti iure dolores,
+        facilis dignissimos illum tempora autem cupiditate distinctio fugiat
+        quasi officiis provident fugit. Iste, ea! Doloribus deserunt itaque in
+        neque voluptate quas reprehenderit et, id numquam dolorum minima quod
+        porro? Cupiditate esse expedita non repudiandae, commodi pariatur odio
+        iste minus, magni sunt libero!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        libero sint soluta quae eum fugiat eligendi assumenda nisi? Numquam id
+        aliquid rerum quam aspernatur expedita officia voluptatum tempore ipsa
+        esse. Numquam doloribus reprehenderit rem incidunt, velit saepe
+        excepturi animi, laborum molestiae nesciunt temporibus provident.
+        Deleniti consectetur vel amet dolorum sapiente, voluptatibus officiis,
+        eos numquam mollitia a culpa magnam fugiat molestiae.
+      </p>
+    </div>
+  );
 }
